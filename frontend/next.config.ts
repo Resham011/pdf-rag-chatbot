c625d7665f0bd754ec8e543d236f784cb5f8eca5
Resham011 <reshamgithub@gmail.com>
2026-02-15 00:13:56 +0530

    update backend URL

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
-   const nextConfig: NextConfig = {
-     async rewrites() {
-       return [
-         {
-           source: '/api/:path*',
-           destination: 'http://127.0.0.1:8000/api/:path*',
-         },
-       ];
-     },
-     reactStrictMode: false,
-   };
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://askmypdf-backend-8k4o.onrender.com/api/:path*',
+      },
+    ];
+  },
+  reactStrictMode: false,
+};
 
-   export default nextConfig;
+export default nextConfig;
